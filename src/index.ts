b06605ac5bae21b995be789bbb5998fdfe014041
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-12-14 23:20:35 +0800

    chore: init

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { createExtension } from '@vscode-use/utils'
+
+export = createExtension(() => {
+// eslint-disable-next-line no-console
+  console.log('Simon Plugin Collection is now active!')
+}, () => {
+
+})
